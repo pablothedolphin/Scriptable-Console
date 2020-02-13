97a496207ed54fcfa48ad6128d2133b4239f6274
Jakir Hussain <jakir.hussain@hotmail.co.uk>
2020-02-13 17:20:07 +0000

    0.2.3: Wrote parsers to convert strings of a defined schema into various struct types for later development.

---
+++ Assets/Scriptable Console/API/Scripts/CommandArguments.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ScriptableFramework.DeveloperConsole
+{
+	public static class CommandArguments
+	{
+		public static bool TryParse (string argument, out Vector2 value)
+		{
+			value = Vector2.zero;
+
+			if (!TryStripBrackets(argument, out string strippedArgument)) return false;
+
+			if (!TrySplitComponents (strippedArgument, 2, out string[] components)) return false;
+
+			if (!float.TryParse (components[0], out float x) || 
+				!float.TryParse (components[1], out float y)) return false;
+
+			value = new Vector2 (x, y);
+
+			return true;
+		}
+
+		public static bool TryParse (string argument, out Vector3 value)
+		{
+			value = Vector3.zero;
+
+			if (!TryStripBrackets (argument, out string strippedArgument)) return false;
+
+			if (!TrySplitComponents (strippedArgument, 3, out string[] components)) return false;
+
+			if (!float.TryParse (components[0], out float x) || 
+				!float.TryParse (components[1], out float y) ||
+				!float.TryParse (components[2], out float z)) return false;
+
+			value = new Vector3 (x, y, z);
+
+			return true;
+		}
+
+		public static bool TryParse (string argument, out Bounds value)
+		{
+			value = new Bounds ();
+
+			if (!TryStripBrackets (argument, out string strippedArgument)) return false;
+
+			string[] vectors = strippedArgument.Split ("),(".ToCharArray ());
+
+			if (vectors.Length != 2) return false;
+
+			vectors[0] = vectors[0] + ")";
+			vectors[1] = "(" + vectors[1];
+
+			if (!TryParse (vectors[0], out Vector3 center)) return false;
+			if (!TryParse (vectors[1], out Vector3 size)) return false;
+
+			value = new Bounds (center, size);
+
+			return true;
+		}
+
+		private static bool TryStripBrackets (string argument, out string strippedArgument)
+		{
+			strippedArgument = "";
+
+			if (!argument.StartsWith ("(") || !argument.EndsWith (")")) return false;
+
+			strippedArgument = argument.Remove (0).Remove (argument.Length - 1);
+
+			return true;
+		}
+
+		private static bool TrySplitComponents (string strippedArgument, int componentCount, out string[] components)
+		{
+			components = new string[0];
+
+			string[] tempComponents = strippedArgument.Split (',');
+
+			if (tempComponents.Length != componentCount) return false;
+
+			components = tempComponents;
+
+			return true;
+		}
+	}
+}
